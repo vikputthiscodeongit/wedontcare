2331d7a17ea44ab7ae0c28826686dcf158a9e86b
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2021-05-02 12:19:09 +0200

    Change indentation

--- header.php
+++ header.php
@@ -1,38 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!--
+    <head>
+        <!-- Character set -->
+        <meta charset="utf-8">
 
-    - - - - - - - - - - - - - - - - - - - -
-    - This website is made by Viktor Chin -
-    - - - - - - - viktorchin.nl - - - - - -
+        <!--
 
-    -->
+        - - - - - - - - - - - - - - - - - - - -
+        - This website is made by Viktor Chin -
+        - - - - - - - viktorchin.nl - - - - - -
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        -->
 
-    <!-- Site information -->
-    <title>WE DON'T CARE</title>
-    <meta name="description" content="WE DON'T CARE">
-    <meta name="keywords" content="We Don't Care Productions">
+        <!-- Viewport scaling -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Required site assets -->
-    <?php
-      $style_version = date("Ymd_His", filemtime("dist/css/style.css"));
-    ?>
-    <link rel="stylesheet" type="text/css" href="/dist/css/style.css?ver=<?php echo $style_version; ?>">
+        <!-- Site information -->
+        <title>WE DON'T CARE</title>
+        <meta name="description" content="WE DON'T CARE">
+        <meta name="keywords" content="We Don't Care Productions">
 
-    <!-- Favicon stuff -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/dist/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/dist/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/dist/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/dist/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/dist/favicon/safari-pinned-tab.svg" color="#000000">
-    <meta name="msapplication-TileColor" content="#000000">
-    <meta name="theme-color" content="#000000">
-  </head>
+        <!-- Site assets -->
+        <?php
+            $style_version = date("Ymd_His", filemtime("dist/css/style.css"));
+        ?>
+        <link rel="stylesheet" type="text/css" href="/dist/css/style.css?ver=<?php echo $style_version; ?>">
 
-  <body>
-    <main>
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/dist/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/dist/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/dist/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/dist/favicon/site.webmanifest">
+        <link rel="mask-icon" href="/dist/favicon/safari-pinned-tab.svg" color="#000000">
+        <meta name="msapplication-TileColor" content="#000000">
+        <meta name="theme-color" content="#000000">
+    </head>
+
+    <body>
+        <main>
--- index.php
+++ index.php
@@ -1,45 +1,45 @@
 <?php include "header.php"; ?>
 
 <div class="nav-item">
-  <a class="nav-link text text--flashy" href="https://shop.wedontcaregroup.com" target="_self">Webshop</a>
+    <a class="nav-link text text--flashy" href="https://shop.wedontcaregroup.com" target="_self">Webshop</a>
 </div>
 
 <div class="nav-item">
-  <a class="nav-link" id="nav-link-latest" href="https://latest.wedontcaregroup.com" target="_self">Latest</a>
+    <a class="nav-link" id="nav-link-latest" href="https://latest.wedontcaregroup.com" target="_self">Latest</a>
 
-  <div class="rollout" id="rollout-latest" aria-describedby="nav-link-latest">
-    <p>Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.</p>
-  </div>
+    <div class="rollout" id="rollout-latest" aria-describedby="nav-link-latest">
+        <p>Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.</p>
+    </div>
 </div>
 
 <div class="nav-item">
-  <a class="nav-link" href="#" target="_blank">YouTube</a>
+    <a class="nav-link" href="#" target="_blank">YouTube</a>
 </div>
 
 <div class="nav-item">
-  <a class="nav-link" href="#" target="_blank">Instagram</a>
+    <a class="nav-link" href="#" target="_blank">Instagram</a>
 </div>
 
 <section class="content">
-  <div class="spinning-logo">
-    <div class="aspect-ratio aspect-ratio--square" aria-hidden="true">
-      <div class="content">
-        <video poster="https://via.placeholder.com/600x400">Your browser sucks and does not support awesomeness.</video>
-      </div>
+    <div class="spinning-logo">
+        <div class="aspect-ratio aspect-ratio--square" aria-hidden="true">
+            <div class="content">
+                <video poster="https://via.placeholder.com/600x400">Your browser sucks and does not support awesomeness.</video>
+            </div>
+        </div>
+
+        <span class="sr-only">
+            We Don't Care
+        </span>
     </div>
 
-    <span class="sr-only">
-      We Don't Care
-    </span>
-  </div>
-
-  <form class="form form--mailing" id="form-mailing">
-    <div class="form__field form__field--inline-send">
-      <label class="form__label sr-only" for="mailing-email">Enter your email to stay up to date</label>
-      <input class="form__input" type="mailing-email" name="mailing-email" id="mailing-email" placeholder="Enter your email to stay up to date">
-      <button class="form__submit btn" type="submit">Sign up</button>
-    </div>
-  </form>
+    <form class="form form--width-small" id="form-mailing">
+        <div class="form__field form__field--inline-send">
+            <label class="form__label sr-only" for="mailing-email">Enter your email to stay up to date</label>
+            <input class="form__input" type="mailing-email" name="mailing-email" id="mailing-email" placeholder="Enter your email to stay up to date">
+            <button class="form__submit btn" type="submit">Sign up</button>
+        </div>
+    </form>
 </section>
 
 <?php include "footer.php"; ?>
