36321eb3bc394dc9ce00ac9548ffad7f70d1c247
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2021-05-13 16:07:23 +0200

    Create initial mailing sign up back end logic

---
+++ mailing-signup.php
@@ -0,0 +1,25 @@
+<?php
+if (!empty($_POST["email"])) {
+    $host = "127.0.0.1";
+    $db   = "wdc-landing";
+    $user = "root";
+    $pass = "";
+    $port = "3306";
+    $charset = "utf8mb4";
+
+    $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+    try {
+        $pdo = new \PDO($dsn, $user, $pass, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+
+    header( "Location: {$_SERVER['REQUEST_URI']}", true, 303 );
+
+    exit();
+}
