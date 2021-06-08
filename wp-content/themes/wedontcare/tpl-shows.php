aebe2574e0b324a0d2ac759b30c7a77f0157b879
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2021-06-08 18:43:47 +0200

    Create "Show" custom post type

--- wp-content/themes/wedontcare/tpl-shows.php
+++ wp-content/themes/wedontcare/tpl-shows.php
@@ -8,26 +8,52 @@
     <h1>Shows</h1>
 
     <?php
-        $shows = get_field("shows")["show"];
-        // var_dump($shows);
-    ?>
-    <?php
-        if ($shows) {
-            foreach($shows as $show) {
-                // var_dump($show);
+        $query_args = array(
+            "post_type" => "show",
+            "post_status" => "publish",
+            "posts_per_page" => -1,
+            "order" => "ASC"
+        );
 
-                $date_raw = $show["date"];
-                $date_pretty = date("d M", strtotime($show["date"]));
-                ?>
-                <div class="show">
-                    <a class="show__link" href="<?php echo $show["url"]; ?>" target="_blank" rel="noopener">
-                        <time datetime="<?php echo $date_raw; ?>"><?php echo $date_pretty; ?></time>
-                        <span class="seperator"> - </span>
-                        <span><?php echo $show["venue"]; ?></span>
-                    </a>
-                </div>
+        $the_query = new WP_Query($query_args);
+
+        if ($the_query->have_posts()) {
+            ?>
+            <ul class="show__list">
                 <?php
-            }
+                    while ($the_query->have_posts()) {
+                        $the_query->the_post();
+
+                        $attrs = get_field("show_attrs");
+                        // var_dump($attrs);
+
+                        // Tag
+                        $tag_open = "span class='show'";
+                        $tag_close = "span";
+
+                        if (!empty($attrs["url"])) {
+                            $tag_open = "a class='show' href='" . $attrs["url"] . "' target='_blank' rel='noopener'";
+                            $tag_close = "a";
+                        }
+
+                        // Date
+                        $date_raw = $attrs["date"];
+                        $date_pretty = date("d M", strtotime($attrs["date"]));
+                        ?>
+                        <li class="show__item">
+                            <<?php echo $tag_open; ?>>
+                                <time datetime="<?php echo $date_raw; ?>"><?php echo $date_pretty; ?></time>
+                                <span class="seperator"> - </span>
+                                <span><?php echo $attrs["venue"]; ?></span>
+                            </<?php echo $tag_close; ?>>
+                        </li>
+                        <?php
+                    }
+
+                    wp_reset_postdata();
+                ?>
+            </ul>
+            <?php
         }
     ?>
 </div>
