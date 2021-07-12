b6aa94046fd28b046973ec5292af5f420b153314
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2021-07-13 00:55:43 +0200

    Finish "Social"

--- wp-content/themes/wedontcare/tpl-social.php
+++ wp-content/themes/wedontcare/tpl-social.php
@@ -6,99 +6,97 @@
 
 <div class="container container--align-center">
     <div class="row row--space-center row--lg-align-center">
-        <?php
-            $query_args = array(
-                "post_type" => "entity",
-                "post_status" => "publish",
-                "posts_per_page" => 3,
-                "order" => "ASC"
-            );
+        <ul class="socials">
+            <?php
+                $query_args = array(
+                    "post_type" => "entity",
+                    "post_status" => "publish",
+                    "posts_per_page" => 3,
+                    "order" => "ASC"
+                );
 
-            $the_query = new WP_Query($query_args);
+                $the_query = new WP_Query($query_args);
 
-            if ($the_query->have_posts()) {
-                while ($the_query->have_posts()) {
-                    $the_query->the_post();
+                if ($the_query->have_posts()) {
+                    while ($the_query->have_posts()) {
+                        $the_query->the_post();
 
-                    $entity = get_field("entity");
-                    // var_dump($entity);
+                        $entity = get_field("entity");
+                        // var_dump($entity);
 
-                    $classes = "box box--4";
+                        // Social
+                        $social = $entity["social"];
+                        // var_dump($social);
 
-                    $id = $entity["id"];
+                        if (!empty($social)) {
+                            $services = $social["service"];
 
-                    if (!empty($id))
-                        $classes .= " box--" . $id;
+                            foreach ($services as $service) {
+                                $link = $service["url"];
+                                $name = false;
+                                $logo = false;
 
-                    // Social
-                    $social = $entity["social"];
-                    // var_dump($social);
+                                // Name
+                                switch ($service["id"]) {
+                                    case "youtube":
+                                        $name = "YouTube";
 
-                    if (!empty($social)) {
-                        $services = $social["service"];
+                                        break;
+                                    default:
+                                        $name = ucwords($service["id"]);
+                                }
 
-                        foreach ($services as $service) {
-                            $link = $service["url"];
-                            $name = false;
-                            $logo = false;
+                                if (!$name)
+                                    return;
 
-                            // Name
-                            switch ($service["id"]) {
-                                case "youtube":
-                                    $name = "YouTube";
+                                // var_dump($name);
 
-                                    break;
-                                default:
-                                    $name = ucwords($service["id"]);
-                            }
+                                // Logo
+                                $base_dir  = trailingslashit(THEME_DIR_PATH);
+                                $dir       = "dist/images/static/social/";
+                                $file_name = $service["id"] . "-white.png";
+                                $files     = glob($base_dir . $dir . $file_name);
+                                // var_dump($files);
 
-                            // var_dump($name);
+                                if (count($files) === 1)
+                                    $logo = get_theme_file_uri($dir . basename($files[0]));
 
-                            // Logo
-                            $base_dir  = trailingslashit(THEME_DIR_PATH);
-                            $dir       = "dist/images/static/social/";
-                            $file_name = $service["id"] . "-white.png";
-                            $files     = glob($base_dir . $dir . $file_name);
-                            // var_dump($files);
-
-                            if (count($files) === 1)
-                                $logo = get_theme_file_uri($dir . basename($files[0]));
-
-                            // var_dump($logo);
-                            ?>
-                            <div class="<?php echo $classes; ?>">
-                                <?php
-                                    if ($logo) {
-                                        ?>
-                                        <div class="media" style="--aspect-ratio: 1 / 1">
-                                            <img src="<?php echo $logo; ?>" alt="<?php echo $name; ?> logo">
-
-                                            <a class="stretched-link" href="<?php echo $link; ?>" target="_blank" rel="noopener"></a>
-                                        </div>
-                                        <?php
-                                    }
-
-                                    else if ($name) {
-                                        ?>
-                                        <span><?php echo $name; ?></span>
-                                        <?php
-                                    }
+                                // var_dump($logo);
                                 ?>
-                            </div>
-                            <?php
+                                <li class="social">
+                                    <?php
+                                        if ($logo) {
+                                            ?>
+                                            <div class="media" style="--aspect-ratio: 1 / 1">
+                                                <img src="<?php echo $logo; ?>" alt="<?php echo $name; ?> logo">
+
+                                                <a class="stretched-link" href="<?php echo $link; ?>" target="_blank" rel="noopener">
+                                                    <span class="sr-only"><?php echo $name; ?></span>
+                                                </a>
+                                            </div>
+                                            <?php
+                                        } else {
+                                            ?>
+                                            <a href="<?php echo $link; ?>" target="_blank" rel="noopener"><?php echo $name; ?></a>
+                                            <?php
+                                        }
+                                    ?>
+                                </li>
+                                <?php
+                            }
                         }
                     }
-                }
 
-                wp_reset_postdata();
-            } else {
-                ?>
-                <div class="text text--center">
-                    <p>Error message.</p>
-                </div>
-                <?php
-            }
-        ?>
+                    wp_reset_postdata();
+                } else {
+                    ?>
+                    <div class="text text--center">
+                        <p>Error message.</p>
+                    </div>
+                    <?php
+                }
+            ?>
+        </ul>
     </div>
 </div>
 
